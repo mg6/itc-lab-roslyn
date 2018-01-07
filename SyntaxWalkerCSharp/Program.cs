7166551c7718fed42851402e125db9bc9c416b44
Maciej Gamrat <maciej@gamrat.it>
2018-01-07 23:14:52 +0100

    Add example code for visitor project

--- SyntaxWalkerCSharp/Program.cs
+++ SyntaxWalkerCSharp/Program.cs
@@ -15,6 +15,37 @@ namespace SyntaxWalkerCSharp
     {
         static void Main(string[] args)
         {
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(
+            @"using System;
+            using System.Collections.Generic;
+            using System.Linq;
+            using System.Text;
+            using Microsoft.CodeAnalysis;
+            using Microsoft.CodeAnalysis.CSharp;
+
+            namespace TopLevel
+            {
+                using Microsoft;
+                using System.ComponentModel;
+
+                namespace Child1
+                {
+                    using Microsoft.Win32;
+                    using System.Runtime.InteropServices;
+
+                    class Foo { }
+                }
+
+                namespace Child2
+                {
+                    using System.CodeDom;
+                    using Microsoft.CSharp;
+
+                    class Bar { }
+                }
+            }");
+
+            var root = tree.GetRoot() as CompilationUnitSyntax;
         }
     }
 }
