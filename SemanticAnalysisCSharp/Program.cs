0b5f58c859715967dfbcacbce215ff45118d007b
Maciej Gamrat <maciej@gamrat.it>
2018-01-08 00:30:41 +0100

    Add example code for semantic analysis project

--- SemanticAnalysisCSharp/Program.cs
+++ SemanticAnalysisCSharp/Program.cs
@@ -15,6 +15,24 @@ namespace SemanticAnalysisCSharp
     {
         static void Main(string[] args)
         {
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(
+                @"using System;
+                using System.Collections;
+                using System.Linq;
+                using System.Text;
+
+                namespace HelloWorld
+                {
+                    class Program
+                    {
+                        static void Main(String[] args)
+                        {
+                            Console.WriteLine(""Hello world!"");
+                        }
+                    }
+                }");
+
+            var root = tree.GetRoot() as CompilationUnitSyntax;
         }
     }
 }
