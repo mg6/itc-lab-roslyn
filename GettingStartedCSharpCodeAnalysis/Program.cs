82a04078dc529f37d4d34a202cea5504c40dad09
Maciej Gamrat <maciej@gamrat.it>
2018-01-07 23:04:45 +0100

    Add node querying example

--- GettingStartedCSharpCodeAnalysis/Program.cs
+++ GettingStartedCSharpCodeAnalysis/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using Microsoft.CodeAnalysis;
@@ -15,7 +16,37 @@ namespace GettingStartedCSharpCodeAnalysis
     {
         static void Main(string[] args)
         {
-            var workspace = MSBuildWorkspace.Create();
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(
+                @"using System;
+                using System.Collections;
+                using System.Linq;
+                using System.Text;
+
+                namespace HelloWorld
+                {
+                    class Program
+                    {
+                        static void Main(String[] args)
+                        {
+                            Console.WriteLine(""Hello world!"");
+                        }
+                    }
+                }");
+
+            // manual traversal
+            var root = tree.GetRoot() as CompilationUnitSyntax;
+            var firstElement = root.Members[0] as NamespaceDeclarationSyntax;
+            var programDeclaration = firstElement.Members[0] as ClassDeclarationSyntax;
+            var mainDeclaration = programDeclaration.Members[0] as MethodDeclarationSyntax;
+            var arrayParameterByManual = mainDeclaration.ParameterList.Parameters[0];
+
+            // querying with LINQ
+            var firstParameters = from methodDeclaration in root.DescendantNodes().OfType<MethodDeclarationSyntax>()
+                                  where methodDeclaration.Identifier.ValueText == "Main"
+                                  select methodDeclaration.ParameterList.Parameters.First();
+            var arrayParameterByLinq = firstParameters.Single();
+
+            Debug.Assert(arrayParameterByManual == arrayParameterByLinq);
         }
     }
 }
