d4fe4f4a6fdae2947e59caf7aff5d4b9912ab8d6
Maciej Gamrat <maciej@gamrat.it>
2018-01-08 03:00:40 +0100

    Setup type inference rewriter for project files

--- TransformationCSharp/Program.cs
+++ TransformationCSharp/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using Microsoft.CodeAnalysis;
@@ -15,6 +16,47 @@ namespace TransformationCSharp
     {
         static void Main(string[] args)
         {
+            Compilation test = CreateTestCompilation();
+
+            foreach (SyntaxTree sourceTree in test.SyntaxTrees)
+            {
+                SemanticModel model = test.GetSemanticModel(sourceTree);
+
+                TypeInferenceRewriter rewriter = new TypeInferenceRewriter(model);
+                SyntaxNode newSource = rewriter.Visit(sourceTree.GetRoot());
+
+                if (newSource != sourceTree.GetRoot())
+                {
+                    File.WriteAllText(sourceTree.FilePath, newSource.ToFullString());
+                }
+            }
+        }
+
+        private static Compilation CreateTestCompilation()
+        {
+            String programPath = @"Program.cs";
+            String programText = File.ReadAllText(programPath);
+            SyntaxTree programTree = CSharpSyntaxTree.ParseText(programText)
+                .WithFilePath(programPath);
+
+            String rewriterPath = @"TypeInferenceRewriter.cs";
+            String rewriterText = File.ReadAllText(rewriterPath);
+            SyntaxTree rewriterTree = CSharpSyntaxTree.ParseText(rewriterText)
+                .WithFilePath(rewriterPath);
+
+            SyntaxTree[] sourceTrees = { programTree, rewriterTree };
+
+            MetadataReference mscorlib = MetadataReference.CreateFromFile(
+                typeof(object).Assembly.Location);
+            MetadataReference codeAnalysis = MetadataReference.CreateFromFile(
+                typeof(SyntaxTree).Assembly.Location);
+            MetadataReference cSharpCodeAnalysis = MetadataReference.CreateFromFile(
+                typeof(CSharpSyntaxTree).Assembly.Location);
+
+            MetadataReference[] references = { mscorlib, codeAnalysis, cSharpCodeAnalysis };
+
+            return CSharpCompilation.Create("TransformationCS",
+                sourceTrees, references, new CSharpCompilationOptions(OutputKind.ConsoleApplication));
         }
     }
 }
