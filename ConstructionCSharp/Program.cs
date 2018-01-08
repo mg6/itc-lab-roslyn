40631803cd135919d020f7e4f398f149455e845c
Maciej Gamrat <maciej@gamrat.it>
2018-01-08 01:57:32 +0100

    Compose QualifiedNames out of IdentifierNames

--- ConstructionCSharp/Program.cs
+++ ConstructionCSharp/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using Microsoft.CodeAnalysis;
@@ -8,6 +9,7 @@ using Microsoft.CodeAnalysis.CSharp.Symbols;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.MSBuild;
 using Microsoft.CodeAnalysis.Text;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace ConstructionCSharp
 {
@@ -15,6 +17,17 @@ namespace ConstructionCSharp
     {
         static void Main(string[] args)
         {
+            NameSyntax name = IdentifierName("System");
+            Debug.Assert(name.ToString() == "System");
+
+            name = QualifiedName(name, IdentifierName("Collections"));
+            Debug.Assert(name.ToString() == "System.Collections");
+
+            name = QualifiedName(name, IdentifierName("Generic"));
+            Debug.Assert(name.ToString() == "System.Collections.Generic");
+
+            var nonExistentName = IdentifierName("NonExistent");
+            Debug.Assert(nonExistentName.ToString() == "NonExistent");
         }
     }
 }
